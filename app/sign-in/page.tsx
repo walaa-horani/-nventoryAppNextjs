dd07ad7a0acd14e602e8cd7355a65fc6b559a53d
Your Name <walaa.horani@hotmail.com>
2025-10-27 09:40:15 +0300

    update sign-in redirect

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,19 +1,22 @@
-import { SignIn } from '@stackframe/stack'
-import Link from 'next/link'
-import React from 'react'
+"use client";
 
-function SignInPage() {
+import { SignIn, useUser } from "@stackframe/stack";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function SignInPage() {
+  const user = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/dashboard");
+    }
+  }, [user, router]);
+
   return (
-    <div className='flex items-center justify-center min-h-screen flex-col mx-auto bg-linear-to-br from-purple-50 to-purple-100'>
-       
-      
-        
-        <SignIn/>
-
-       
-         
+    <div className="flex items-center justify-center min-h-screen flex-col mx-auto bg-linear-to-br from-purple-50 to-purple-100">
+      <SignIn />
     </div>
-  )
+  );
 }
-
-export default SignInPage
