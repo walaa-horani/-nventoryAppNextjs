9b5117330f59e0e3cfb988ef066edb29f1f0dfd8
Your Name <walaa.horani@hotmail.com>
2025-12-20 02:10:27 +0300

    Add .npmrc to fix deployment peer dependency issues

--- stack/server.tsx
+++ stack/server.tsx
@@ -4,4 +4,7 @@ import { StackServerApp } from "@stackframe/stack";
 
 export const stackServerApp = new StackServerApp({
   tokenStore: "nextjs-cookie",
+  projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID!,
+  publishableClientKey: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY!,
+  secretServerKey: process.env.STACK_SECRET_SERVER_KEY!,
 });
